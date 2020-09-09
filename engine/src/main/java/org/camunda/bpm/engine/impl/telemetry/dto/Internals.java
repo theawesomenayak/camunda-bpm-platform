f91ab9cdbdfeacbac618528daa1a327b99d5a2ee
Yana Vasileva <yana.vasileva@camunda.com>
2020-09-09 17:14:18 +0200

    chore(telemetry): extend telemetry data with integration flags
    * spring boot starter
    * camunda bpm run
    * wildfly/jboss subsystem
    * ejb-service
    
    Related to CAM-12227

--- engine/src/main/java/org/camunda/bpm/engine/impl/telemetry/dto/Internals.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/telemetry/dto/Internals.java
@@ -24,11 +24,14 @@ import com.google.gson.annotations.SerializedName;
 public class Internals {
 
   public static final String SERIALIZED_APPLICATION_SERVER = "application-server";
+  public static final String SERIALIZED_CAMUNDA_INTEGRATION = "camunda-integration";
 
   protected Database database;
   @SerializedName(value = SERIALIZED_APPLICATION_SERVER)
   protected ApplicationServer applicationServer;
   protected Map<String, Command> commands;
+  @SerializedName(value = SERIALIZED_CAMUNDA_INTEGRATION)
+  protected Map<String, Object> camundaIntegration;
 
   protected Map<String, Metric> metrics;
 
@@ -40,6 +43,7 @@ public class Internals {
     this.database = database;
     this.applicationServer = server;
     this.commands = new HashMap<>();
+    this.camundaIntegration = new HashMap<>();
   }
 
   public Internals(Internals internals) {
@@ -78,6 +82,14 @@ public class Internals {
 
   public void setMetrics(Map<String, Metric> metrics) {
     this.metrics = metrics;
+  }
+
+  public Map<String, Object> getCamundaIntegration() {
+    return camundaIntegration;
+  }
+
+  public void setCamundaIntegration(Map<String, Object> camundaIntegration) {
+    this.camundaIntegration = camundaIntegration;
   }
 
   public void mergeDynamicData(Internals other) {
